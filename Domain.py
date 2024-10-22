6825bac2640dd5972bf4c725468a90c2be54e9b3
idovpn1321 <ridokun199@gmail.com>
2024-10-22 21:37:42 +0700

    Domain.py

--- Domain.py
+++ Domain.py
@@ -1,21 +1,18 @@
 import sublist3r
 import sys
-import os
 
 def find_subdomains(domain):
     # Menggunakan Sublist3r untuk menemukan subdomain
+    # Menyediakan argumen yang diperlukan
     engines = 'Google,Bing,Yahoo'  # Menggunakan string yang dipisahkan koma
     subdomains = sublist3r.main(domain, 40, None, None, False, True, False, engines)
     
-    # Menentukan path untuk menyimpan hasil
-    output_file_path = f'/storage/shared/{domain}_subdomains.txt'  # Menggunakan path yang benar
-    
     # Menyimpan hasil ke dalam file
-    with open(output_file_path, 'w') as f:
+    with open(f'{domain}_subdomains.txt', 'w') as f:
         for subdomain in subdomains:
             f.write(f"{subdomain}\n")
     
-    print(f"Subdomain untuk {domain} telah ditemukan dan disimpan ke {output_file_path}")
+    print(f"Subdomain untuk {domain} telah ditemukan dan disimpan ke {domain}_subdomains.txt")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
